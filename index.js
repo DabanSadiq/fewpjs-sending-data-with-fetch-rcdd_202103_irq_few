d1c024976e94601361ca191ab7b56cffed885103
Daban Sadiq <dabansadiq@icloud.com>
2021-04-15 18:42:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function submitData(name,email){
   }).then((response)=>{
     return response.json()
   }).then((json)=>{
-    console.log(json);
+    // console.log(json);
   }).catch((e)=>{
     console.log(e);
   });
