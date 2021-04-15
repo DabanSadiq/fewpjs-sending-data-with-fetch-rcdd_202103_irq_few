444f8bd18ca546e7465c06946c8d6eb510f730c6
Daban Sadiq <dabansadiq@icloud.com>
2021-04-15 18:38:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Add your code here
+function submitData(name,email){
+
+  fetch("http://localhost:3000/dogs", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      "name": name,
+      "email": email,
+    })
+  });
+
+}
