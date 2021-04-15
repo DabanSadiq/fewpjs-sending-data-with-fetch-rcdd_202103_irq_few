310b1ec3e403d52d98b1eb99c53cfe4ddb3937d0
Daban Sadiq <dabansadiq@icloud.com>
2021-04-15 19:04:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Add your code here
 function submitData(name,email){
 
-   fetch("http://www.localhost:3000/users", {
+  return fetch("http://localhost:3000/users", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -21,4 +21,4 @@ function submitData(name,email){
 
 }
 
-const r = submitData("daban sadiq","daban@gmail.com");
+const r = submitData("daban","mail");
